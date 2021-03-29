a092e717f5b1d1d6c01e3cb700140efc1fc1a2bb
tejaswi <avula.usedu1@gmail.com>
2021-03-28 19:47:35 -0500

    added bag

--- routes/handbag.js
+++ routes/handbag.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('handbag', { title: 'Search Results handbag' });
+  res.render('handbag', { title: 'Search Results Handbag' });
 });
 
 module.exports = router;
