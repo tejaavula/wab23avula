4f50770a4262da23b2e2efb2f28085df813d12e5
tejaswi <avula.usedu1@gmail.com>
2021-03-31 16:09:25 -0500

    added slot

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var handbagRouter=require('./routes/handbag');
-var starsRouter=require('./routes/stars');
+var slotRouter=require('./routes/slot');
 
 
 var app = express();
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/handbag',handbagRouter);
-app.use('/stars',starsRouter);
+app.use('/slot',slotRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
